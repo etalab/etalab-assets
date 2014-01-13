dfaaa7d5e6b7b0fb486dcf3bd71eb299ac2abe29
Axel Haustant <axel@data.gouv.fr>
2014-01-13 17:51:21 +0100

    Added missing territory kind

--- js/etalab-site.js
+++ js/etalab-site.js
@@ -11,6 +11,7 @@
             'CommuneOfFrance',
             'Country',
             'DepartmentOfFrance',
+            'MetropoleOfCountry',
             'OverseasCollectivityOfFrance',
             'RegionOfFrance'
         ],
